e67fd931fe0c17cb49fd88a178d1c719e6aa0d56
nipil <nipil@users.noreply.github.com>
2026-03-28 10:28:23 +0000

    Minimal index head

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+// #![deny(warnings)]
+
+use anyhow::Result;
+
+const BASE_URL: &str = "https://packs.download.microchip.com";
+const INDEX: &str = "index.idx";
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // default client with connection pooling
+    let client = reqwest::Client::new();
+
+    // process pack index
+    let url = format!("{BASE_URL}/{INDEX}");
+
+    // getting etag
+    eprintln!("HEAD {url:?}...");
+    let res = client.head(url).send().await?;
+    eprintln!("Response: {:?} {}", res.version(), res.status());
+    eprintln!("Headers: {:#?}\n", res.headers());
+
+    // let body = res.text().await?;
+    Ok(())
 }
