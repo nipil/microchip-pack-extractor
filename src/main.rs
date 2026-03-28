88585d1addce89a721cc2ef1fccc617875cb09d6
nipil <nipil@users.noreply.github.com>
2026-03-28 10:44:18 +0000

    Extract etag

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,29 @@
-// #![deny(warnings)]
-
-use anyhow::Result;
-
 const BASE_URL: &str = "https://packs.download.microchip.com";
 const INDEX: &str = "index.idx";
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() {
     // default client with connection pooling
     let client = reqwest::Client::new();
 
     // process pack index
     let url = format!("{BASE_URL}/{INDEX}");
 
-    // getting etag
-    eprintln!("HEAD {url:?}...");
-    let res = client.head(url).send().await?;
+    // getting head
+    eprintln!("Fetching headers for {url}");
+    let res = client.head(url).send().await;
+    let res = res.expect("FIXME");
     eprintln!("Response: {:?} {}", res.version(), res.status());
     eprintln!("Headers: {:#?}\n", res.headers());
 
+    // extracting etag
+    let etag = res.headers().get("ETag");
+    let etag = etag.expect("FIXME");
+    let etag = etag.to_str();
+    let etag = etag.expect("FIXME");
+    let etag = quoted_string::strip_dquotes(etag);
+    let etag = etag.expect("FIXME");
+    eprintln!("ETag : {}\n", etag);
+
     // let body = res.text().await?;
-    Ok(())
 }
