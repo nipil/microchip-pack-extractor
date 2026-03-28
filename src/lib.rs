a3eba6775cafcffc6da7b4114b1abd7af5e7c8f7
nipil <nipil@users.noreply.github.com>
2026-03-28 19:11:40 +0000

    Add kv logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use log::{debug, info, trace};
 use serde::Deserialize;
 use serde_xml_rs;
 use std::fs;
@@ -7,7 +8,7 @@ const BASE_URL: &str = "https://packs.download.microchip.com";
 const INDEX_NAME: &str = "index.idx";
 
 async fn head_url(client: &reqwest::Client, url: &str) -> reqwest::Response {
-    eprintln!("Fetching headers for {url}");
+    debug!(url=url; "Fetching headers");
     client
         .head(url)
         .send()
@@ -18,7 +19,7 @@ async fn head_url(client: &reqwest::Client, url: &str) -> reqwest::Response {
 }
 
 async fn get_url(client: &reqwest::Client, url: &str) -> reqwest::Response {
-    eprintln!("Fetching content for {url}");
+    debug!(url=url; "Fetching content");
     client
         .get(url)
         .send()
@@ -35,22 +36,8 @@ fn get_etag_from_response(res: &reqwest::Response) -> &str {
         .expect("Index headers must contain an ETag")
         .to_str()
         .expect("ETag must be convertible to string");
+    trace!(etag=etag; "Found ETag in headers");
     quoted_string::strip_dquotes(etag).expect("Etag must be quoted")
-}
-
-fn get_cached_etag_bytes(name: &str, etag: &str) -> Option<Vec<u8>> {
-    let cache = format!("{etag}.{name}");
-    if let Ok(content) = fs::read(&cache) {
-        eprintln!("Reusing cached content from {cache}");
-        return Some(content);
-    }
-    eprintln!("No cache available for {cache}");
-    None
-}
-
-fn set_cached_etag_bytes(name: &str, etag: &str, content: &Vec<u8>) {
-    let cache = format!("{etag}.{name}");
-    fs::write(cache, &content).expect("Writing to cache must not fail");
 }
 
 async fn get_cached_url_content_by_etag(
@@ -61,13 +48,15 @@ async fn get_cached_url_content_by_etag(
     // detect newest version using ETag
     let res = head_url(client, url).await;
     let etag = get_etag_from_response(&res);
-    eprintln!("Most recent header Etag for {url} is {etag}");
+    debug!(url=url, etag=etag; "Lookup most recent etag for url");
 
     // return local cache if any is available
-    let cache = get_cached_etag_bytes(cache_file, etag);
-    if let Some(content) = cache {
+    let cache_file = format!("{etag}.{cache_file}");
+    if let Ok(content) = fs::read(&cache_file) {
+        debug!(cache=cache_file.as_str(), size=content.len(); "Reading cache");
         return content;
     }
+    debug!(cache=cache_file.as_str(); "No cache available");
 
     // get latest content
     let res = get_url(client, url).await;
@@ -75,8 +64,10 @@ async fn get_cached_url_content_by_etag(
     let content = res.bytes().await.expect("Index must have content");
     let content = Vec::from(content);
 
-    // save content to cache
-    set_cached_etag_bytes(cache_file, &etag, &content);
+    // save content to cache (TOC/TOU: ETag might changed inbetween)
+    let cache_file = format!("{etag}.{cache_file}");
+    debug!(cache=cache_file.as_str(), size=content.len(); "Writing cache");
+    fs::write(cache_file, &content).expect("Writing to cache must not fail");
 
     content
 }
@@ -85,8 +76,10 @@ pub async fn pack_index(client: &reqwest::Client) -> Idx {
     let url = format!("{BASE_URL}/{INDEX_NAME}");
     let content = get_cached_url_content_by_etag(client, &url, INDEX_NAME).await;
     let content = str::from_utf8(&content).expect("Index should be valid utf-8 text");
-    eprintln!("Parsing Index...");
-    serde_xml_rs::from_str(&content).expect("Index XML should parse correctly")
+    info!("Parsing Index...");
+    let index: Idx = serde_xml_rs::from_str(&content).expect("Index XML should parse correctly");
+    debug!(size=index.pdscs.len(); "Index size");
+    index
 }
 
 #[derive(Deserialize)]
@@ -105,7 +98,7 @@ impl Idx {
 
     pub async fn process_dfps(&self, client: &reqwest::Client) {
         let dfps = self.dpf_pdsc();
-        eprintln!("Processing {} device packs", dfps.len());
+        info!(count=dfps.len(); "Processing device packs");
         type TaskResult = (Atpack, Vec<u8>);
         let mut tasks: Vec<JoinHandle<TaskResult>> = vec![];
         // spawn one task per dfp
@@ -113,21 +106,18 @@ impl Idx {
             let atpack = dfp.atpack();
             let new_client = client.clone();
             tasks.push(tokio::spawn(async move {
-                eprintln!("Processing DFP: {}", atpack.file_name);
+                info!(name=atpack.file_name.as_str(); "Started pack");
                 let content =
                     get_cached_url_content_by_etag(&new_client, &atpack.url, &atpack.file_name)
                         .await;
+                debug!(name=atpack.file_name.as_str(); "Pack size");
                 (atpack, content)
             }));
         }
         // wait for each task to complete
         for task in tasks {
-            let (atpack, content) = task.await.expect("Processing DFP should not fail");
-            eprintln!(
-                "Size of device-family-pack {} is {} bytes",
-                atpack.file_name,
-                content.len()
-            );
+            let (atpack, content) = task.await.expect("Processing pack should not fail");
+            info!(name=atpack.file_name.as_str(); "Finished pack");
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use log::info;
 use microchip_pack_extractor as mpe;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    info!("Starting up !");
     let client = reqwest::Client::new();
     let index = mpe::pack_index(&client).await;
     index.process_dfps(&client).await;
