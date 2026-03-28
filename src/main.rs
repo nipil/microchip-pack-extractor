79e79f6053ad8627acd754bc2efa05cf77232f59
nipil <nipil@users.noreply.github.com>
2026-03-28 18:24:04 +0000

    Initialize logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use log::info;
 use microchip_pack_extractor as mpe;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+    info!("Starting up !");
     let client = reqwest::Client::new();
     let index = mpe::pack_index(&client).await;
     index.process_dfps(&client).await;
