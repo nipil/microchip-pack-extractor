9b5f0f5a657ddda6cccada13757d4611ba2c0312
nipil <nipil@users.noreply.github.com>
2026-03-29 09:10:59 +0000

    Create a command for fetching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use microchip_pack_extractor as mpe;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Fetch,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let client = reqwest::Client::new();
-    let index = mpe::pack_index(&client).await;
-    index.process_dfps(&client).await;
+    let args = Args::parse();
+    match args.command {
+        Commands::Fetch => {
+            let client = reqwest::Client::new();
+            let index = mpe::pack_index(&client).await;
+            index.process_dfps(&client).await;
+        }
+    }
 }
