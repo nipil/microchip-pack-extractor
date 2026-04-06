ab8c6fe52f22be1ba8e960604152f9165aa724fb
nipil <nipil@users.noreply.github.com>
2026-04-06 08:06:49 +0000

    Process provided futures concurrently, 3x speed increase

--- src/main.rs
+++ src/main.rs
@@ -27,16 +27,19 @@ async fn main() {
     // get the latest index
     let idx = index::Idx::fetch(&web_cache).await;
 
+    // set some limits
+    let limit_fetch = num_cpus::get_physical();
+    let limit_processing = num_cpus::get();
+
     // process each pdsc using a nice iter syntactic sugar to keep Idx opaque
     stream::iter(&idx)
         // to prefetch the content (from cache or web)
         .map(|pdsc| async { pdsc.fetch(&web_cache).await })
         // run at most N concurrent futures
         // process them in whichever order they finish first
-        .buffer_unordered(num_cpus::get_physical())
+        .buffer_unordered(limit_fetch)
         // then feed them to a new task
-        .for_each(|archive| async move {
-            // use associated function because we move it to a rayon thread
+        .for_each_concurrent(limit_processing, |archive| async move {
             package::PdscArchive::process(archive).await;
         })
         // wait for all tasks to complete
